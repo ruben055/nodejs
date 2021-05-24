0f94e36d8afaee2557068aa1b8f43f74b1b8ca62
ruben siregar <rubensiregar055@gmail.com>
2021-05-24 16:25:29 +0900

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+ res.send('Halo Node.js!');
+});
+
+app.listen(3000, function () {
+  console.log('aplikasi berjalan pada port 3000!');
+});
