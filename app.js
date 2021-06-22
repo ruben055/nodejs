3db298aef9fc6af102f3e934d10de313149bebf2
Ruben Mual Siregar <63962557+ruben055@users.noreply.github.com>
2021-06-22 21:45:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
- res.send('Halo Node.js!');
+ res.send('Halo Node.js1!');
 });
 
 app.listen(3000, function () {
